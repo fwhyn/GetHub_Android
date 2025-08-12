7133b33da01267f3942a3b7bcc9ceea1aa31bc43
fwhyn <fwhynseventh@gmail.com>
2025-08-12 12:35:32 +0700

    Fake Variant
    added implementations

--- app/src/fake/kotlin/com/fwhyn/app/gethub/feature/func/user/data/remote/GitHubUsersResponse.kt
+++ app/src/fake/kotlin/com/fwhyn/app/gethub/feature/func/user/data/remote/GitHubUsersResponse.kt
@@ -8,24 +8,19 @@ import okhttp3.mockwebserver.RecordedRequest
 
 object GitHubUsersResponse {
     fun getOrNull(request: RecordedRequest): MockResponse? {
-        if (request.path?.startsWith("/users") != true) {
-            return null
-        }
-
+        val url = request.requestUrl
+        val urlSegments = url?.pathSegments
+        val users = urlSegments?.getOrNull(0)
         val perPage = GitHubQueryParam.perPageParam(request)
-        val since = GitHubQueryParam.sinceParam(request)
+        if (urlSegments?.size != 1 || users != "users" || perPage != 20) return null
 
+        val since = GitHubQueryParam.sinceParam(request)
         val successResponse = MockResponse().setResponseCode(200)
 
-        return when {
-            perPage == null || since == null -> successResponse.setBody(success_item20_since0)
-            perPage == 20 -> when (since) {
-                0 -> successResponse.setBody(success_item20_since0)
-                30 -> successResponse.setBody(success_item20_since30)
-                70 -> successResponse.setBody(success_item20_since70)
-                else -> successResponse.setBody("[]")
-            }
-
+        return when (since) {
+            0 -> successResponse.setBody(success_item20_since0)
+            30 -> successResponse.setBody(success_item20_since30)
+            70 -> successResponse.setBody(success_item20_since70)
             else -> successResponse.setBody("[]")
         }
     }
