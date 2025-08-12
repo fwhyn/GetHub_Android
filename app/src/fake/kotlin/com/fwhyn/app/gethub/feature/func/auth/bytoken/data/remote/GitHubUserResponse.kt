2fcb6323a0471fd6f4943a769d154bc6717d800a
fwhyn <fwhynseventh@gmail.com>
2025-08-12 12:39:29 +0700

    Fake Variant
    added implementations

--- app/src/fake/kotlin/com/fwhyn/app/gethub/feature/func/auth/bytoken/data/remote/GitHubUserResponse.kt
+++ app/src/fake/kotlin/com/fwhyn/app/gethub/feature/func/auth/bytoken/data/remote/GitHubUserResponse.kt
@@ -6,9 +6,10 @@ import okhttp3.mockwebserver.RecordedRequest
 
 object GitHubUserResponse {
     fun getOrNull(request: RecordedRequest): MockResponse? {
-        if (request.path != "/user") {
-            return null
-        }
+        val url = request.requestUrl
+        val urlSegments = url?.pathSegments
+        val user = urlSegments?.getOrNull(0)
+        if (urlSegments?.size != 1 || user != "user") return null
 
         return MockResponse()
             .setResponseCode(200)
