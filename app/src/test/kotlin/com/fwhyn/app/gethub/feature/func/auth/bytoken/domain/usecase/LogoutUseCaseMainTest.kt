db9529b14e612d8c33b075aca3c7824f7e0d9888
fwhyn <fwhynseventh@gmail.com>
2025-08-17 21:09:06 +0700

    Tests
    LogoutUseCaseMainTest.kt

---
+++ app/src/test/kotlin/com/fwhyn/app/gethub/feature/func/auth/bytoken/domain/usecase/LogoutUseCaseMainTest.kt
@@ -0,0 +1,23 @@
+package com.fwhyn.app.gethub.feature.func.auth.bytoken.domain.usecase
+
+import MainDispatcherRule
+import org.junit.After
+import org.junit.Before
+import org.junit.Rule
+
+class LogoutUseCaseMainTest {
+
+    @get:Rule
+    val mainDispatcherRule = MainDispatcherRule()
+
+    @Before
+    fun setUp() {
+
+    }
+
+    @After
+    fun tearDown() {
+
+    }
+
+}
