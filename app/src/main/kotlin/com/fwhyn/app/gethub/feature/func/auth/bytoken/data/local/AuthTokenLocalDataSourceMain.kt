2fdb82605dcd81c17bb94c45a79f74c1f2a9acc4
fwhyn <fwhynseventh@gmail.com>
2025-08-17 14:14:56 +0700

    DataStore, Tests
    added implementations

--- app/src/main/kotlin/com/fwhyn/app/gethub/feature/func/auth/bytoken/data/local/AuthTokenLocalDataSourceMain.kt
+++ app/src/main/kotlin/com/fwhyn/app/gethub/feature/func/auth/bytoken/data/local/AuthTokenLocalDataSourceMain.kt
@@ -4,20 +4,17 @@ import androidx.datastore.core.DataStore
 import com.fwhyn.app.gethub.feature.func.auth.bytoken.data.model.AuthTokenData
 import com.fwhyn.app.gethub.feature.func.datastore.data.model.UserPrefs
 import com.google.gson.Gson
-import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.first
 import kotlinx.coroutines.flow.map
-import kotlinx.coroutines.flow.stateIn
 import javax.inject.Inject
-import kotlin.coroutines.coroutineContext
 
 class AuthTokenLocalDataSourceMain @Inject constructor(
     private val encryptedUserPrefs: DataStore<UserPrefs>
 ) : AuthTokenLocalDataSource {
 
     override suspend fun get(): AuthTokenData? {
-        val stateFlow = getFlow().stateIn(CoroutineScope(coroutineContext))
-        return stateFlow.value
+        return getFlow().first()
     }
 
     override fun getFlow(): Flow<AuthTokenData?> {
