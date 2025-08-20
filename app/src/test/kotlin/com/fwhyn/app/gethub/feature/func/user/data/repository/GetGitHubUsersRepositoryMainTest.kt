317f33183af39628cbb06f24c7cc291717b2693b
fwhyn <fwhynseventh@gmail.com>
2025-08-20 14:00:31 +0700

    GetGitHubUsersRepository Test
    added test implementations

--- app/src/test/kotlin/com/fwhyn/app/gethub/feature/func/user/data/repository/GetGitHubUsersRepositoryMainTest.kt
+++ app/src/test/kotlin/com/fwhyn/app/gethub/feature/func/user/data/repository/GetGitHubUsersRepositoryMainTest.kt
@@ -1,12 +1,15 @@
 package com.fwhyn.app.gethub.feature.func.user.data.repository
 
 import MainDispatcherRule
+import com.fwhyn.app.gethub.feature.func.auth.bytoken.data.remote.GitHubAuthResponse.LIMITED_TOKEN_FAKE
 import com.fwhyn.app.gethub.feature.func.auth.bytoken.data.remote.GitHubAuthResponse.TOKEN_FAKE
 import com.fwhyn.app.gethub.feature.func.auth.bytoken.data.remote.MockWebServerProvider
 import com.fwhyn.app.gethub.feature.func.auth.bytoken.di.RetrofitGitHubDiMain
 import com.fwhyn.app.gethub.feature.func.user.data.model.GetGitHubUsersRepoParam
 import com.fwhyn.app.gethub.feature.func.user.data.model.GitHubUserData
 import com.fwhyn.app.gethub.feature.func.user.di.GitHubUsersDiMain
+import com.fwhyn.lib.baze.common.model.Exzeption
+import com.fwhyn.lib.baze.common.model.Status
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.test.runTest
 import org.junit.After
@@ -52,22 +55,55 @@ class GetGitHubUsersRepositoryMainTest {
     fun `users increased with perPage number each time called`() = runTest {
         token = TOKEN_FAKE
 
-        var result: List<GitHubUserData>? = getUsers(this)
-        Assert.assertEquals(perPage, result?.size)
+        var users: List<GitHubUserData>? = getUsers(this)
+        Assert.assertEquals(perPage, users?.size)
 
-        result = getUsers(this)
-        Assert.assertEquals(perPage * 2, result?.size)
+        users = getUsers(this)
+        Assert.assertEquals(perPage * 2, users?.size)
+    }
+
+    @Test
+    fun `no user returned when load with invalid token`() = runTest {
+        token = ""
+
+        val users = getUsers(this)
+        Assert.assertEquals(null, users?.size)
+    }
+
+    @Test
+    fun `unauthorized error returned when load with invalid token`() = runTest {
+        token = ""
+
+        unAuthorizedError(this)
+    }
+
+    @Test
+    fun `unauthorized error returned when load with limited token`() = runTest {
+        token = LIMITED_TOKEN_FAKE
+        unAuthorizedError(this)
+    }
+
+    private suspend fun unAuthorizedError(scope: CoroutineScope) {
+        val result = getResult(scope)
+        var error: Exzeption? = null
+        result.onFailure { error = it as? Exzeption }
+        Assert.assertEquals(Status.Unauthorized, error?.status)
     }
 
     private suspend fun getUsers(scope: CoroutineScope): List<GitHubUserData>? {
-        var result: List<GitHubUserData>? = null
+        var result = getResult(scope)
+        return result.getOrNull()
+    }
+
+    private suspend fun getResult(scope: CoroutineScope): Result<List<GitHubUserData>> {
+        var result: Result<List<GitHubUserData>> = Result.failure(Exception())
         getGitHubUsersRepository.invoke(
             scope = scope,
             onFetchParam = {
                 GetGitHubUsersRepoParam(perPage = perPage)
             },
             onOmitResult = {
-                result = it.getOrNull()
+                result = it
             }
         )
         getGitHubUsersRepository.join()
