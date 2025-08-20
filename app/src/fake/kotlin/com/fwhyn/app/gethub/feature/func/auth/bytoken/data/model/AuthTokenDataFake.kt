2662e3c01a85f8bae30f3a5def03f421490578b6
fwhyn <fwhynseventh@gmail.com>
2025-08-20 10:16:41 +0700

    LogoutUseCase Test
    added test implementations

--- app/src/fake/kotlin/com/fwhyn/app/gethub/feature/func/auth/bytoken/data/model/AuthTokenDataFake.kt
+++ app/src/fake/kotlin/com/fwhyn/app/gethub/feature/func/auth/bytoken/data/model/AuthTokenDataFake.kt
@@ -2,9 +2,10 @@ package com.fwhyn.app.gethub.feature.func.auth.bytoken.data.model
 
 import com.fwhyn.app.gethub.feature.func.auth.bytoken.data.remote.GitHubAuthResponse.TOKEN_FAKE
 
-val authTokenDataFake = AuthTokenData(
+
+val unvalidatedAuthTokenDataFake = AuthTokenData(
     value = TOKEN_FAKE,
-    validatedUser = authUserDataFake
+    validatedUser = null
 )
 
-val unvalidatedAuthTokenDataFake = authTokenDataFake.copy(validatedUser = null)
+val authTokenDataFake = unvalidatedAuthTokenDataFake.copy(validatedUser = authUserDataFake)
--- app/src/test/kotlin/com/fwhyn/app/gethub/feature/func/auth/bytoken/domain/usecase/LogoutUseCaseMainTest.kt
+++ app/src/test/kotlin/com/fwhyn/app/gethub/feature/func/auth/bytoken/domain/usecase/LogoutUseCaseMainTest.kt
@@ -1,23 +1,73 @@
 package com.fwhyn.app.gethub.feature.func.auth.bytoken.domain.usecase
 
 import MainDispatcherRule
-import org.junit.After
-import org.junit.Before
+import com.fwhyn.app.gethub.common.helper.extension.StatusExt
+import com.fwhyn.app.gethub.feature.func.auth.bytoken.data.local.AuthTokenLocalDataSourceFake
+import com.fwhyn.app.gethub.feature.func.auth.bytoken.data.model.authTokenDataFake
+import com.fwhyn.app.gethub.feature.func.auth.bytoken.data.repository.AuthTokenRepository
+import com.fwhyn.app.gethub.feature.func.auth.bytoken.data.repository.AuthTokenRepositoryMain
+import com.fwhyn.lib.baze.common.helper.Util
+import com.fwhyn.lib.baze.common.model.Exzeption
+import io.mockk.coEvery
+import io.mockk.mockk
+import kotlinx.coroutines.test.runTest
+import org.junit.Assert
 import org.junit.Rule
+import org.junit.Test
 
 class LogoutUseCaseMainTest {
 
     @get:Rule
     val mainDispatcherRule = MainDispatcherRule()
 
-    @Before
-    fun setUp() {
+    private lateinit var logoutUseCase: LogoutUseCase
 
+    @Test
+    fun `token will be cleared when logout success`() = runTest {
+        val authTokenLocalDataSource = AuthTokenLocalDataSourceFake()
+        val authTokenRepository = AuthTokenRepositoryMain(authTokenLocalDataSource)
+        logoutUseCase = LogoutUseCaseMain(authTokenRepository)
+
+        authTokenLocalDataSource.set(authTokenDataFake)
+        var actualAuthTokenData = authTokenLocalDataSource.get()
+        Assert.assertEquals(authTokenDataFake, actualAuthTokenData)
+
+        logoutUseCase.invoke(
+            scope = this,
+            onFetchParam = {},
+            onOmitResult = {
+                it.onSuccess {
+                    actualAuthTokenData = authTokenLocalDataSource.get()
+                }.onFailure {
+                    Util.throwMustNotFailed()
+                }
+            }
+        )
+        logoutUseCase.join()
+        Assert.assertEquals(null, actualAuthTokenData)
     }
 
-    @After
-    fun tearDown() {
+    @Test
+    fun `resulting logout error when logout failed`() = runTest {
+        val authTokenRepository = mockk<AuthTokenRepository>(relaxed = true)
+        logoutUseCase = LogoutUseCaseMain(authTokenRepository)
 
+        coEvery { authTokenRepository.set(Unit, null) } answers { throw Exception() }
+
+        var error: Exzeption? = null
+        logoutUseCase.invoke(
+            scope = this,
+            onFetchParam = {},
+            onOmitResult = {
+                it.onSuccess {
+                    Util.throwMustNotSuccess()
+                }.onFailure { err ->
+                    error = err as? Exzeption
+                }
+            }
+        )
+        logoutUseCase.join()
+
+        Assert.assertEquals(StatusExt.LogoutError, error?.status)
     }
-
 }
