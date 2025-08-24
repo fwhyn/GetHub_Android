6a39a1369d335673cfc5824e92d7e9c370404315
fwhyn <fwhynseventh@gmail.com>
2025-08-24 21:47:06 +0700

    Test
    hilt and robo electric test

---
+++ app/src/test/kotlin/com/fwhyn/app/gethub/UnitTest.kt
@@ -0,0 +1,34 @@
+package com.fwhyn.app.gethub
+
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import dagger.hilt.android.testing.HiltAndroidRule
+import dagger.hilt.android.testing.HiltAndroidTest
+import dagger.hilt.android.testing.HiltTestApplication
+import org.junit.Assert
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.robolectric.annotation.Config
+
+@RunWith(AndroidJUnit4::class)
+@Config(
+    application = HiltTestApplication::class,
+    maxSdk = 36
+)
+@HiltAndroidTest
+class UnitTest {
+
+    @get:Rule
+    var hiltRule = HiltAndroidRule(this)
+
+    @Before
+    fun setUp() {
+        hiltRule.inject()
+    }
+
+    @Test
+    fun test() {
+        Assert.assertEquals(true, true)
+    }
+}
